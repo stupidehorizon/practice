aab4e25dc4b0b790f58c3297d8c5edd39ffd8fab
stupidehorizon <122505231@qq.com>
2019-04-26 22:06:44 +0800

    promise 支持了异步调用

--- promise/index.js
+++ promise/index.js
@@ -6,7 +6,7 @@ class P {
   constructor(func) {
     this.state = PENDING;
     this.result = null;
-
+    this.eventQueue = [];
     this.resolve = this.resolve.bind(this);
     this.reject = this.reject.bind(this);
     this.then = this.then.bind(this);
@@ -16,18 +16,25 @@ class P {
   resolve(result) {
      this.state = RESOLVE;
      this.result = result;
+     this.eventQueue.forEach(([successHandle, failHandle]) => {
+      successHandle && successHandle(result);
+     });
   }
 
   reject(result) {
      this.state = REJECT;
      this.result = result;
+     this.eventQueue.forEach(([successHandle, failHandle]) => {
+      failHandle && failHandle(result);
+     });
   }
 
   then(successHandle, failHandle) {
     switch(this.state) {
       case RESOLVE: successHandle && successHandle(this.result); break;
       case REJECT: failHandle && failHandle(this.result); break;
-    }
+      case PENDING: this.eventQueue.push([successHandle,failHandle]); break;
+    };
   }
 
   finally(finallyResolve) {
@@ -40,7 +47,9 @@ class P {
 }
 
 p = new P(function (resolve, reject){
-  resolve('promise resolve');
+  setTimeout(() => {
+    resolve('promise resolve');
+  }, 2000);
 })
 
 p.then(console.log);
